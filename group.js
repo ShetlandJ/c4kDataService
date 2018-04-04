dcd769decf3824bd67c649499d71e78cafd80c07
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-04-04 17:04:34 +0100

    changed class structure

--- group.js
+++ group.js
@@ -1,16 +1,19 @@
-function Group(courseName, capacity, students, waitingList) {
-  this.courseName = courseName;
-  this.capacity = capacity;
-  this.students = students;
-  this.waitingList = waitingList;
-}
+class Group {
 
-Group.prototype.remainingClassSpace = function() {
-  var classSpace = this.capacity - this.students.length
-  if (classSpace > 0) {
-    return classSpace
-  } else {
-    return 0;
+  constructor(courseName, capacity, students, waitingList) {
+    this.courseName = courseName;
+    this.capacity = capacity;
+    this.students = students;
+    this.waitingList = waitingList;
+  }
+
+  remainingClassSpace() {
+    var classSpace = this.capacity - this.students.length
+    if (classSpace > 0) {
+      return classSpace
+    } else {
+      return 0;
+    }
   }
 }
 
